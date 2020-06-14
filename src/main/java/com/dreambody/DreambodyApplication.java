5cce93cdcec38c813007fa2bbdb764d6460f15ba
yoon pyo, hong <ghddbsvy@gmail.com>
2020-06-14 15:43:07 +0900

    Initial commit

---
+++ src/main/java/com/dreambody/DreambodyApplication.java
@@ -0,0 +1,13 @@
+package com.dreambody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreambodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreambodyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dreambody/DreambodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreambody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreambodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
