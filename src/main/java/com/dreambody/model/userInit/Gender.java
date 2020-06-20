419f0f643fae4fc9e0a31ae89a1ab1218806f07a
monkeyDugi <lbd4946@gmail.com>
2020-06-21 02:11:31 +0900

    활동성, 성별, 목표 Entity 생성

---
+++ src/main/java/com/dreambody/model/userInit/Gender.java
@@ -0,0 +1,34 @@
+package com.dreambody.model.userInit;
+
+import com.dreambody.model.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * @author : 이병덕
+ * @description : 사용자가 초기에 정보 생성 시 선택하는 성별
+ * @date : 2020.06.21
+ */
+
+@Getter
+@NoArgsConstructor
+@Table(name ="genders", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "gender")
+})
+@Entity
+public class Gender extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 1)
+    private String gender;
+
+    @Builder
+    public Gender (String gender) {
+        this.gender = gender;
+    }
+}
