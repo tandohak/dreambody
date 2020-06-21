f250b1b76a9df89eb766c18ca7b0b6f7c4955da5
monkeyDugi <lbd4946@gmail.com>
2020-06-21 18:39:15 +0900

    식품 api 진행 중

---
+++ src/main/java/com/dreambody/controller/foodApi/FoodInfoController.java
@@ -0,0 +1,4 @@
+package com.dreambody.controller.foodApi;
+
+public class FoodInfoController {
+}
---
+++ src/main/java/com/dreambody/controller/foodApi/dto/FoodInfoResponseDto.java
@@ -0,0 +1,16 @@
+package com.dreambody.controller.foodApi.dto;
+
+import lombok.Getter;
+
+/**
+ * @author : 이병덕
+ * @decription : 식품안전나라 API 응답 Dto
+ * @date : 2020.06.21
+ */
+
+@Getter
+public class FoodInfoResponseDto {
+    int calorie;
+    String food;
+
+}
