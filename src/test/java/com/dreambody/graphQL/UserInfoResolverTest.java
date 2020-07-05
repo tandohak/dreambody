3d279c72cf3217b04b9087fea3affece5c334d1d
yoon pyo, hong <ghddbsvy@gmail.com>
2020-07-05 14:50:37 +0900

    playground cdn version 변경.

--- src/test/java/com/dreambody/graphQL/UserInfoResolverTest.java
+++ src/test/java/com/dreambody/graphQL/UserInfoResolverTest.java
@@ -16,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
